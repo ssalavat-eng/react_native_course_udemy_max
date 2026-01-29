ce8dfc465a77e43681cb0089b1bf21e82b207a48
Salavat Saifullin <ssalavat@gmail.com>
2026-01-29 13:27:52 +0400

    Lesson Navigation

--- App.js
+++ App.js
@@ -1,91 +1,20 @@
-import { useState, useCallback } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts} from "expo-font";
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import Colors from "./constants/colors";
-import GameOverScreen from "./screens/GameOverScreen";
-
-SplashScreen.preventAutoHideAsync();
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [userNumber, setUserNumber] = useState();
-    const [gameIsOver, setGameIsOver] = useState(true);
-    const [guessRounds, setGuessRounds] = useState(0);
-
-    const [fontsLoaded] = useFonts({
-        'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
-        'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf'),
-    });
-
-    // Hide splash screen once fonts are loaded
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
-
-    if(!fontsLoaded){
-        return null; // Don't render anything until fonts are ready
-    }
-
-    function pickedNumberHandler(pickedNumber) {
-        setUserNumber(pickedNumber);
-        setGameIsOver(false);
-    }
-
-    function gameOverHandler(numberOfRounds) {
-        setGameIsOver(true);
-        setGuessRounds(numberOfRounds);
-    }
-
-    function startNewGameHandler() {
-        setUserNumber(null);
-        setGuessRounds(0);
-    }
-
-    let screen = <StartGameScreen onPickNumber = {pickedNumberHandler}/>;
-
-    if(userNumber) {
-        screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
-    }
-
-    if(gameIsOver && userNumber) {
-        screen = <GameOverScreen
-            userNumber={userNumber}
-            roundsNumber={guessRounds}
-            onStartNewGame={startNewGameHandler}
-        />;
-    }
-
-    return (
-        <>
-            <StatusBar style = {'light'}/>
-            <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen} onLayout={onLayoutRootView}>
-                <ImageBackground
-                    source={require('./assets/images/background.png')}
-                    resizeMode={"cover"}
-                    style={styles.rootScreen}
-                    imageStyle={styles.backgroundImage}
-                >
-                    <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-
-                </ImageBackground>
-            </LinearGradient>
-        </>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    rootScreen: {
-        flex: 1,
-    },
-    backgroundImage: {
-        opacity: 0.15,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
