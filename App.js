8a977598b4879324d80fc4265507f05a49ac14b5
Salavat Saifullin <ssalavat@gmail.com>
2026-01-29 14:46:19 +0400

    99 root parameters to pass data

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <StatusBar style={"dark"} />
+            <NavigationContainer>
+                <Stack.Navigator id={"topMain"} initialRouteName="MealsCategories" component={CategoriesScreen} >
+                    <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+                    <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
