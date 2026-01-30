7291e3997d8f2483d29874a3be1336bcde75253f
Salavat Saifullin <ssalavat@gmail.com>
2026-01-30 16:14:58 +0400

    109-111 Drawer and Tab navigation

--- App.js
+++ App.js
@@ -1,41 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
-import CategoriesScreen from "./screens/CategoriesScreen";
-import MealsOverviewScreen from "./screens/MealsOverviewScreen";
-import MealDetailsScreen from "./screens/MealDetailsScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+
+const Drawer = createDrawerNavigator();
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
-    return (
-        <>
-            <StatusBar style={"light"} />
-            <NavigationContainer>
-                <Stack.Navigator id={"topMain"} initialRouteName="MealsCategories" component={CategoriesScreen}
-                                 screenOptions={{
-                                     headerStyle: { backgroundColor: '#351401' },
-                                     headerTintColor: 'white',
-                                     contentStyle: { backgroundColor: '#3f2f25' },
-                                 }} >
-                    <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{
-                        title: 'All Categories',
-                    }} />
-                    <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-                    <Stack.Screen name="MealDetail" component={MealDetailsScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </>
-    );
-}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+  return (
+//          <NavigationContainer>
+//              <Drawer.Navigator initialRouteName="User" screenOptions={{
+//                  headerStyle: { backgroundColor: '#3c0a6b' },
+//                  headerTintColor: 'white',
+//                  drawerActiveBackgroundColor: '#f0e1ff',
+//                  drawerActiveTintColor: '#3c0a6b',
+//              }}>
+//                  <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
+//                      drawerLabel: 'Welcome Screen',
+//                      drawerIcon: ({ color, size }) => <Ionicons name={"home"} color={color} size={size} />
+//                  }}/>
+//                  <Drawer.Screen name="User" component={UserScreen}
+//                                 options={{
+//                                     drawerIcon: ({ color, size }) => <Ionicons name={"person"} color={color} size={size} />
+//                                 }}/>
+//              </Drawer.Navigator>
+//          </NavigationContainer>
+          <NavigationContainer>
+              <BottomTab.Navigator initialRouteName="User" screenOptions={{
+                  headerStyle: { backgroundColor: '#3c0a6b' },
+                  headerTintColor: 'white',
+                  tabBarActiveTintColor: '#3c0a6b',
+              }} id={'1'}>
+                  <BottomTab.Screen 
+                      name="Welcome" 
+                      component={WelcomeScreen} 
+                      options={{
+                          tabBarIcon: ({ color, size }) => (
+                            <Ionicons name={"home"} color={color} size={size} />
+                        ),
+                  }} />
+                  <BottomTab.Screen 
+                      name="User" 
+                      component={UserScreen} 
+                      options={{
+                          tabBarIcon: ({ color, size }) => (
+                            <Ionicons name={"person"} color={color} size={size} />
+                        ),
+                  }} />
+              </BottomTab.Navigator>
+          </NavigationContainer>
+  );
+}
