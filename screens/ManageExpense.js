4e9b26c01cc031c1342c085cba57913901f31720
Salavat Saifullin <ssalavat@gmail.com>
2026-02-06 20:57:47 +0400

    Section 8 finished (146)

---
+++ screens/ManageExpense.js
@@ -0,0 +1,95 @@
+import {useContext, useLayoutEffect} from "react";
+import {View, StyleSheet} from 'react-native';
+
+import  Button from "../components/UI/Button";
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
+
+function ManageExpense({ route, navigation }) {
+    const expensesCtx = useContext(ExpensesContext);
+
+    const editedExpenseId = route.params?.expenseId; // ?checks if params set
+    const isEditing = !!editedExpenseId; // falsy or truthy
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense',
+        });
+    },[navigation, isEditing]);
+
+    function deleteExpenseHandler(){
+        expensesCtx.deleteExpense(editedExpenseId);
+        navigation.goBack();
+    }
+
+    function cancelHandler(){
+        navigation.goBack();
+    }
+
+    function confirmHandler(){
+        if(isEditing){
+            expensesCtx.updateExpense(
+                editedExpenseId,
+                {
+                    description: 'Test!!!',
+                    amount: 15.87,
+                    date: new Date('2026-01-19')
+                });
+        } else {
+            expensesCtx.addExpense(
+                {
+                    description: 'Test',
+                    amount: 10.87,
+                    date: new Date('2026-02-03')
+                });
+        }
+        navigation.goBack();
+    }
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode={"flat"} onPress={cancelHandler}>Cancel</Button>
+                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update':'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon={"trash"}
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+        </View>
+    );
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 2,
+        backgroundColor: GlobalStyles.colors.primary800,
+    },
+    buttons:{
+        marginTop: 20,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: "center",
+    }
+});
