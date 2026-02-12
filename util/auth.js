03aff41d416263f4149612bc70fac1e6a1dce28f
Salavat Saifullin <ssalavat@gmail.com>
2026-02-12 14:14:53 +0400

    User authentication (172-187)

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyA99TqxAbf9BG5SHF18pw4208UIDKy0jq4';
+const API_KEY = '';
 
 export async function authenticate(mode, email, password) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
